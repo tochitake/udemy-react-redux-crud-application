950a2584877d4c08c4dde45e7335dfe607ce91b4
takeyoshi tochihara <take.tochi0405@gmail.com>
2021-11-03 15:31:21 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
 
 function App() {
-  return <div><h1>hello world!</h1></div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="button" onClick={() => {console.log("i am clicked")}} />
+    </React.Fragment>
+  )
+
 }
 
 export default App;
