5ab1e531933b2bfa4e838bb9b337542eccb23cc0
takeyoshi tochihara <take.tochi0405@gmail.com>
2021-11-03 16:39:47 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import userEvent from "@testing-library/user-event";
 import React, { useReducer } from "react";
+import PropTypes from 'prop-types';
 
 /*function App() {
   return (
@@ -15,7 +16,7 @@ const App = () => {
   const profiles = [
     {name: "Taro", age:10}, 
     {name: "Hanako", age:6},
-    {name: "Kenji"}
+    {name: "Takeshi", age:12}
   ]
   return (
   <div>
@@ -32,8 +33,9 @@ const User = (props) => {
   return <div>HI, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age:1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
