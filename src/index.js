54063c0fefa134514822a6cb73961a11518596a9
takeyoshi tochihara <take.tochi0405@gmail.com>
2021-11-07 15:02:50 +0900

    'state'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
 import './index.css';
-import App from './App';
+import reducers from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
+import { ReactReduxContext } from 'react-redux';
+
+const store = createStore(reducer)
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
